da76ae3f2d21a24e06c64dee2109a454e7b87672
toba101 <toba.obiwale@outlook.com>
2021-01-13 19:13:11 +0000

    completed

--- week02/the.py
+++ week02/the.py
@@ -5,6 +5,7 @@ verb = input('choose a verb: ')
 exclamatory = input('choose an exclamatory: ')
 verb = input('choose a verb: ')
 verb = input('choose a verb: ')
+print(f'\n')
 
 # Output user input
 print(f'adjective: {adjective}')
@@ -16,7 +17,7 @@ print(f'verb: {verb}')
 print(f'\n')
 
 # Print out story
-print(f"""Your story is: \n \n The other day, I was really in trouble. It all started when I saw a very 
+print(f"""Your story is: \n \nThe other day, I was really in trouble. It all started when I saw a very 
 {adjective} {animal} {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
 to do was to {verb} over and over. Miraculously, that caused it to stop, but no 
 before it tried to {verb} right in front of my family.!""")
