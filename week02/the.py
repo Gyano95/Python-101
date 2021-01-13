cae5470f013ccdc6eb5672b02e6fa883cd501047
toba101 <toba.obiwale@outlook.com>
2021-01-13 17:01:03 +0000

    comma correction

--- week02/the.py
+++ week02/the.py
@@ -17,7 +17,7 @@ print(f'\n')
 
 # Print out story
 print(f"""Your story is: \n \n The other day, I was really in trouble. It all started when I saw a very 
-{adjective}, {animal}, {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
+{adjective} {animal} {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
 to do was to {verb} over and over. Miraculously, that caused it to stop, but no 
 before it tried to {verb} right in front of my family.!""")
 
