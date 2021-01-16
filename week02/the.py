d0b1503cf896dd721757dcdd8ed3ce680d931f2e
toba101 <toba.obiwale@outlook.com>
2021-01-16 00:26:59 +0000

    second Mad-lib

--- week02/the.py
+++ week02/the.py
@@ -1,24 +1,68 @@
- #--------MAD LIB - WORD GAME---------#
+#  #--------MAD LIB - WORD GAME 01---------#
+# adjective = input('choose an adjective: ')
+# animal = input('choose animal: ')
+# verb = input('choose a verb: ')
+# exclamatory = input('choose an exclamatory: ')
+# verb = input('choose a verb: ')
+# verb = input('choose a verb: ')
+# print(f'\n')
+# # Output user input
+# print(f'adjective: {adjective}')
+# print(f'animal: {animal} ')
+# print(f'verb: {verb}')
+# print(f'exclamatory: {exclamatory}')
+# print(f'verb: {verb}')
+# print(f'verb: {verb}')
+# print(f'\n')
+# # Print out story
+# print(f"""Your story is: \n \nThe other day, I was really in trouble. It all started when I saw a very 
+# {adjective} {animal} {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
+# to do was to {verb} over and over. Miraculously, that caused it to stop, but no 
+# before it tried to {verb} right in front of my family.!""")
+
+#--------Extra Mile----------------------#
+
+# Love is a {} of the body and min
+ 
+ 
+ 
+ #--------MAD LIB - WORD GAME 02---------#
 adjective = input('choose an adjective: ')
-animal = input('choose animal: ')
-verb = input('choose a verb: ')
-exclamatory = input('choose an exclamatory: ')
-verb = input('choose a verb: ')
-verb = input('choose a verb: ')
+nationality = input('choose nationality: ')
+name = input('choose a name: ')
+noun = input('choose a noun: ')
+adjective = input('choose a adjective: ')
+noun = input('choose a noun: ')
+adjective = input('choose a adjective: ')
+adjective = input('choose a adjective: ')
+noun = input('choose a noun: ')
+noun = input('choose a noun: ')
+number = input('choose a number: ')
+plural_shape = input('choose a plural shape: ')
+food = input('choose a food: ')
+food = input('choose a food: ')
 print(f'\n')
 
 # Output user input
 print(f'adjective: {adjective}')
-print(f'animal: {animal} ')
-print(f'verb: {verb}')
-print(f'exclamatory: {exclamatory}')
-print(f'verb: {verb}')
-print(f'verb: {verb}')
+print(f'nationality: {nationality} ')
+print(f'name: {name}')
+print(f'noun: {noun}')
+print(f'adjective: {adjective}')
+print(f'noun: {noun}')
+print(f'adjective: {adjective}')
+print(f'noun: {noun}')
+print(f'noun: {noun} ')
+print(f'number: {number}')
+print(f'plural shape: {plural_shape}')
+print(f'food: {food}')
+print(f'food: {food}')
 print(f'\n')
 
 # Print out story
-print(f"""Your story is: \n \nThe other day, I was really in trouble. It all started when I saw a very 
-{adjective} {animal} {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
-to do was to {verb} over and over. Miraculously, that caused it to stop, but no 
-before it tried to {verb} right in front of my family.!""")
+print(f"""Pizza is invented by a {adjective} {nationality} chef
+named {name}. To make pizza, you need to take a lump of {noun} 
+and make a thin, round {adjective} {noun}.
+Then you cover it with {adjective} sauce {adjective} chesse, and 
+fresh chopped {noun.upper()}!.""")
 
