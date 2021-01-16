3082e6135b8d27c0d3c811386ac70fcbb19cf835
toba101 <toba.obiwale@outlook.com>
2021-01-16 20:26:55 +0000

    g-work

--- examples/roughwork.py
+++ examples/roughwork.py
@@ -1,4 +1,4 @@
-#  Create user input
+# Create user input
 
 first_name = input('What is your first name: ')
 last_name = input('What is your last name: ')
@@ -20,3 +20,22 @@ print(job_title.title())
 print(ID_number)
 print(f'Hair: {hair_color:15} Eyes: {eyes_color}')
 print(f"Month: {month: 14} Training: {training} ")
+
+#------------Data Type and Numbers----------#
+
+birthday = int(input('When is your next birthday: '))
+next_age = birthday + 1
+print(f'Your age would be {next_age}')
+
+
+age = int(input('What is your age '))
+print(f"On your next birthday, you will be, {age + 1}!.")
+print('\n')
+
+carton = int(input('How many egg cartons do you have? '))
+print(f'You have {carton * 12} of eggs.')
+print('\n')
+
+cookies = input('How many cookies do you have?: ')
+people = input('How many people are there? ')
+print(f"Each person may have {int(float(cookies)) / int(float(people))} cookie.")
--- week02/the.py
+++ week02/the.py
@@ -1,4 +1,4 @@
-#  #--------MAD LIB - WORD GAME 01---------#
+#  #--------MAD LIB - WORD GAME Extra---------#
 verb = input('choose an verb: ')
 adverb = input('choose an adverb: ')
 noun = input('choose a noun: ')
@@ -20,12 +20,12 @@ print(f'verb: {verb}')
 print(f'noun: {noun}')
 print(f'\n')
 
-# Print out story
+# Print out story---Extra Mile----#
 print(f"""Your story is: \n \n{verb} your arms {adverb} me and I am {noun}.
 You can't go {noun} and change the {noun} but you can {verb} where
 you are and {verb} the {noun}.""")
 
-#--------Extra Mile----------------------#
+# Example.
 # print(f"""Your story is: \n \nThe other day, I was really in trouble. It all started when I saw a very 
 # {adjective} {animal} {verb} down the hallway. {exclamatory.upper()}. I yelled. but all I could think 
 # to do was to {verb} over and over. Miraculously, that caused it to stop, but no 
