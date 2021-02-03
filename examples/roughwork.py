f0de2f59d7b5c6423239c2813053870fc90b1fc1
toba101 <toba.obiwale@outlook.com>
2021-02-03 23:48:03 +0000

    group-work

--- examples/roughwork.py
+++ examples/roughwork.py
@@ -64,3 +64,4 @@
 radius = float(input("What is the radius of the circle? "))
 area = 3.14 * (radius ** 2)
 print(f"The area of the circle is: {area}")
+
--- week05/g_calculator.py
+++ week05/g_calculator.py
@@ -1,4 +1,5 @@
-print("Enter Toba's Obtained in 5 Subjects: ")
+print("Enter your score in 5 Subjects: ")
+
 gradeOne = int(input('English 101: '))
 gradeTwo = int(input('Math: '))
 gradeThree = int(input('WDD: '))
@@ -8,17 +9,17 @@ gradeFive = int(input('Career Development: '))
 score = gradeOne + gradeTwo + gradeThree + gradeFour + gradeFive
 avg = score / 5
 
-if avg>=91 and avg<=100:
-    print("Your Grade is A+, Excelent!")
-elif avg>=81 and avg<91:
-    print("Your Grade is A-, Great work!")
-elif avg>=71 and avg<81:
-    print("Your Grade is B+, Very Good!")
-elif avg>=61 and avg<71:
-    print("Your Grade is B-, Good Work!")
-elif avg>=51 and avg<61:
-    print("Your Grade is C+, you qualified!")
-elif avg>=41 and avg<51:
+if avg>=90 and avg<=100:
+    print("Your Grade is A, Congratulations! You passed the class!")
+elif avg>=80 and avg<90:
+    print("Your Grade is B+, Great work!")
+elif avg>=70 and avg<80:
+    print("Your Grade is B, Very Good!")
+elif avg>=60 and avg<70:
+    print("Your Grade is C+, Good Work!")
+elif avg>=50 and avg<60:
+    print("Your Grade is C, you qualified!")
+elif avg>=40 and avg<50:
     print("Your Grade is C-, you can still improve!")
 elif avg>=33 and avg<41:
     print("Your Grade is D, Sorry, Final grade is C-!")
@@ -28,3 +29,33 @@ elif avg>=0 and avg<21:
     print("Your Grade is E2, Sorry you failed!")
 else:
     print("Invalid Input!")
+
+    print(f'\n')
+
+# questionOne = input('Are you male? ')
+# questionTwo = input('Are you female? ')
+# absent = input('Do you complete your class? ')
+
+
+
+# print(f'')
+# print('Yes, I am a (questionOne)')
+# print('Yes, I am a (questionTwo)')
+    
+
+
+# #     if student < 0:
+# #         print("Absent")
+# #     else:
+# #         print("You're welcome")
+# # if student == male:
+# #      print('You are a male')
+# # else:
+# #     print("You're Homo") 
+# # if student == female:
+# #     print('You are a female')
+# # else:
+# #     print('You are a gay')
+
+
+           
